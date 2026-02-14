a0cb8766e8159d5d1b0828aa8bdbf216edd66f06
ej-bgnt <ethanbognott@gmail.com>
2026-02-14 11:36:40 +0800

    initial commit

---
+++ intro-to-jsx/src/index.js
@@ -0,0 +1,10 @@
+const React = require("react");
+const ReactDOM = require("react-dom/client");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph.</p>
+  </div>,
+);
