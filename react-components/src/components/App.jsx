afa08d9d968438ee831b8d0d2dc18e54f17776cc
isengard <ethanbognott@gmail.com>
2026-02-20 21:05:37 +0800

    initial commit

---
+++ react-components/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Heading from "./Heading";
+import Unordered_list from "./Unordered-list";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Unordered_list />
+      <Unordered_list />
+    </div>
+  );
+}
+
+export default App;
