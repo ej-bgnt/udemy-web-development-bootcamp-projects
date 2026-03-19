bdaf93596e6f8c8bd3cec0c04e09e1fabeb3fb82
isengard <ethanbognott@gmail.com>
2026-03-19 22:19:48 +0800

    initial commit

---
+++ es6-map-filter-reduce/src/index.js
@@ -0,0 +1,18 @@
+var numbers = [3, 56, 2, 48, 5];
+import emojipedia from "./emojipedia";
+
+//Map -Create a new array by doing something with each item in an array.
+
+//Filter - Create a new array by keeping the items that return true.
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+//Find - find the first item that matches from an array.
+
+//FindIndex - find the index of the first item that matches.
+
+const meaningArr = emojipedia.map(function (i) {
+  return "Meaning " + i.id + ": " + i.meaning.substring(0, 100);
+});
+
+console.log(meaningArr);
